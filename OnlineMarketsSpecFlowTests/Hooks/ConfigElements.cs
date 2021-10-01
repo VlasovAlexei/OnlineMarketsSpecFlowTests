3f91026c077140518bd1d6b1fec18fe7720fd70e
Andrey Vlasov <lion32211@yandex.ru>
2021-10-01 15:03:48 +0300

    Changed the configuration file in accordance with Page Object Strategy

--- OnlineMarketsSpecFlowTests/Hooks/ConfigElements.cs
+++ OnlineMarketsSpecFlowTests/Hooks/ConfigElements.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OnlineMarketsSpecFlowTests.Hooks
 {
     class ConfigElements
     {
-        public string UrlAdress { get; set; }
-        public string SearchField { get; set; }
-        public string SearchButton { get; set; }
-        public string SelectedItemInSearch { get; set; }
-        public string NameOfItem { get; set; }
-        public string AddToBasketButton { get; set; }
-        public string BasketButton { get; set; }
-        public string ItemInBasket { get; set; }
+        public string Url { get; set; }
+
+        public Dictionary<string, Dictionary<string, string>> Pages { get; set; }
     }
 }
