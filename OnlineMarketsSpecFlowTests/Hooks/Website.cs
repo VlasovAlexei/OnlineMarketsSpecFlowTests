3c62bb6f1b00e9400a7bead291cdeef7541da8b7
Andrey Vlasov <lion32211@yandex.ru>
2021-10-03 20:46:13 +0300

    Renamed class ConfigElements to Website. Changed the structure of this file.

---
+++ OnlineMarketsSpecFlowTests/Hooks/Website.cs
@@ -0,0 +1,40 @@
+﻿
+namespace OnlineMarketsSpecFlowTests.Hooks
+{
+    public class Website
+    {
+        public string Url { get; set; }
+        public Pages Pages { get; set; }
+    }
+
+    public class Pages
+    {
+        public StartPage StartPage { get; set; }
+        public ProductListPage ProductListPage { get; set; }
+        public ProductDetailsPage ProductDetailsPage { get; set; }
+        public BasketPage BasketPage { get; set; }
+    }
+
+    public class StartPage
+    {
+        public string SearchField { get; set; }
+    }
+
+    public class ProductListPage
+    {
+        public string SelectedItem { get; set; }
+    }
+
+    public class ProductDetailsPage
+    {
+        public string NameOfItem { get; set; }
+        public string AddToBasketButton { get; set; }
+        public string BasketButton { get; set; }
+    }
+
+    public class BasketPage
+    {
+        public string NameOfItem { get; set; }
+    }
+
+}
